9576a9beacd14d17331563e1d63a2db24e14baaf
karlorg <karlorg@users.noreply.github.com>
2016-12-10 11:09:03 +0000

    z-sorting

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,34 +1,101 @@
 /* globals __DEV__ */
-import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
-import {setResponsiveWidth} from '../utils'
+import Phaser from 'phaser';
 
 export default class extends Phaser.State {
   init () {}
   preload () {}
 
   create () {
-    let banner = this.add.text(this.game.world.centerX, this.game.height - 30, 'Phaser + ES6 + Webpack')
-    banner.font = 'Nunito'
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.anchor.setTo(0.5)
+    // this.palms = ["palm01", "palm02", "palm03", "palm04", "palm05", "palm06"];
+    // this.treesByY = {};
+    for (let row = 0; row < 32; row++) {
+      for (let col = 0; col < 9; col++) {
+        let x = col * 64;
+        const y = row * 16;
+        if (row % 2 === 1) { x += 32; }
+        this.game.add.sprite(x, y, "tile");
+      }
+    }
+    this.zGroup = this.game.add.group();
+    for (let row = 0; row < 32; row++) {
+      for (let col = 0; col < 9; col++) {
+        let x = col * 64;
+        const y = row * 16;
+        if (row % 2 === 1) { x += 32; }
+        // maybe spawn a tree
+        if (this.game.rnd.frac() < 0.15) {
+          const tree = this.game.add.sprite(x + 32, y + 16,
+                                            "palm01");
+          tree.anchor.setTo(130 / tree.width, 104 / tree.height);
+          this.zGroup.add(tree);
+        }
+      }
+    }
+    this.player = this.game.add.sprite(32, 16, "ball");
+    this.player.anchor.setTo(0.5, 1.00);
+    this.zGroup.add(this.player);
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.game.world.centerX,
-      y: this.game.world.centerY,
-      asset: 'mushroom'
-    })
+    this.game.input.gamepad.start();
+  }
 
-    // set the sprite width to 30% of the game width
-    setResponsiveWidth(this.mushroom, 30, this.game.world)
-    this.game.add.existing(this.mushroom)
+  update () {
+    const pad = this.game.input.gamepad.pad1;
+    let targetX = 0;
+    let targetY = 0;
+    if (pad.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) ||
+        pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
+      targetX -= 1;
+    }
+    if (pad.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) ||
+        pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
+      targetX += 1;
+    }
+    if (pad.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) ||
+        pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1) {
+      targetY -= 1;
+    }
+    if (pad.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) ||
+        pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) {
+      targetY += 1;
+    }
+    this.player.x += targetX;
+    this.player.y += targetY;
+
+    this.zGroup.sort('y', Phaser.Group.SORT_ASCENDING);
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    //   this.game.debug.spriteInfo(this.mushroom, 32, 32);
     }
   }
+
+  // registerTree (tree) {
+  //   const y = tree.y;
+  //   if (!this.treesByY.hasOwnProperty(y)) {
+  //     this.treesByY[y] = [];
+  //   }
+  //   this.treesByY[y].push(tree);
+  // }
+
+  // reorderSprites () {
+  //   this.player.kill();
+  //   let stillDead = true;
+  //   for (const y in this.treesByY) {
+  //     if (this.treesByY.hasOwnProperty(y)) {
+  //       const trees = this.treesByY[y];
+  //       for (const tree of trees) {
+  //         tree.kill();
+  //         tree.revive();
+  //       }
+  //       if (this.player.y < y && stillDead) {
+  //         this.player.revive();
+  //         stillDead = false;
+  //       }
+  //     }
+  //   }
+  //   if (stillDead) {
+  //     this.player.revive();
+  //   }
+  // }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -1,23 +1,32 @@
-import Phaser from 'phaser'
-import { centerGameObjects } from '../utils'
+import Phaser from 'phaser';
+import { centerGameObjects } from '../utils';
 
 export default class extends Phaser.State {
   init () {}
 
   preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
+    this.loaderBg = this.add.sprite(
+      this.game.world.centerX, this.game.world.centerY, 'loaderBg');
+    this.loaderBar = this.add.sprite(
+      this.game.world.centerX, this.game.world.centerY, 'loaderBar');
+    centerGameObjects([this.loaderBg, this.loaderBar]);
 
-    this.load.setPreloadSprite(this.loaderBar)
+    this.load.setPreloadSprite(this.loaderBar);
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    this.load.image('tile', 'assets/images/tile.png');
+    this.load.image('ball', 'assets/images/ball.png');
+    this.load.image('palm01', 'assets/images/isometric trees/palm01.png');
+    this.load.image('palm02', 'assets/images/isometric trees/palm02.png');
+    this.load.image('palm03', 'assets/images/isometric trees/palm03.png');
+    this.load.image('palm04', 'assets/images/isometric trees/palm04.png');
+    this.load.image('palm05', 'assets/images/isometric trees/palm05.png');
+    this.load.image('palm06', 'assets/images/isometric trees/palm06.png');
   }
 
   create () {
-    this.state.start('Game')
+    this.state.start('Game');
   }
 
 }
